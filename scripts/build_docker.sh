ee6478e5551842cbde9d750bbaf91577f5a9c03b
江国庆 <guoqingjiang@deepglint.com>
2023-12-01 19:39:55 +0800

    update docker file

--- scripts/build_docker.sh
+++ scripts/build_docker.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 set -ex
-
 root=$(dirname $(dirname $(realpath $0)))
 url=dockerhub.deepglint.com/lse/llava
 tag=app-0.1
+proxy=""
 
 pushd $root
 options=""
-# options="--progress=plain"
-DOCKER_BUILDKIT=1 docker build \
- $options \
- --build-arg proxy="122.97.199.83:17892" \
- -t $url:$tag \
- .
+# options="$options --progress=plain"
+if [ ! -z $proxy ]; then
+    options="$options --build-arg proxy=$proxy
+fi
+cmd="DOCKER_BUILDKIT=1 docker build $options -t $url:$tag ."
+echo $cmd
+$cmd
 popd
